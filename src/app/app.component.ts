998b3a467f2b17f34ba4d7457ac5e64618fe30c9
harip <hariphanisomeshwar@gmail.com>
2021-05-12 16:36:47 -0600

    Palindrome change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'untitled';
+
+  result: boolean;
+  inputString: string;
+
+  constructor() {
+  }
+
+  /**
+   * Check for palindrome
+   * @param value : string
+   */
+  public checkForPalindrome(value: string): void {
+    if (!!!value) {
+      this.result = true;
+      return;
+    }
+
+    const charArray: string[] = value.split('');
+    let startIndex = 0;
+    let endIndex: number = charArray.length - 1;
+
+    while (startIndex < endIndex) {
+      if (charArray[startIndex] !== charArray[endIndex]) {
+        this.result = false;
+        return;
+      }
+
+      startIndex++;
+      endIndex--;
+    }
+
+    this.result = true;
+    return;
+  }
 }
